9c01350b2a8ebfb6db7a9ca5ad1778aab3186815
vigneshblue <vigneshsika@gmail.com>
2016-11-30 01:09:47 +0530

    Created Sign up page with all actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   
-  get 'signup', to:'users#new'
+  
 
   get '/about', to:'static_pages#about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+
+  resources :users
+  get '/signup', to:'users#new'
+  post '/signup', to: 'users#create'
 end
