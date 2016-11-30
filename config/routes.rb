73e6ffb8fb936596269252eabbdd50ac8c682e68
vigneshblue <vigneshsika@gmail.com>
2016-11-30 19:16:53 +0530

    Added login and logout with temporary session

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   
   
-
   get '/about', to:'static_pages#about'
+  get '/help', to:'static_pages#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   resources :users
   get '/signup', to:'users#new'
   post '/signup', to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
